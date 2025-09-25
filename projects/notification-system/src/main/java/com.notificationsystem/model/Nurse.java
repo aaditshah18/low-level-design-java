57f875a650f5d5ddf66c3d67145a28a60b12c038
Aadit <shah.aadit1@northeastern.edu>
2025-09-24 17:43:26 -0700

    Add observer pattern 1. Patient subject to add, remove and notify observers 2. VitalSignMonitor model to checkVitals for a patient and send notification to observers

--- projects/notification-system/src/main/java/com.notificationsystem/model/Nurse.java
+++ projects/notification-system/src/main/java/com.notificationsystem/model/Nurse.java
@@ -2,11 +2,4 @@ package com.notificationsystem.model;
 
 import com.notificationsystem.enums.Role;
 
-public class Nurse extends User {
-    public Nurse(String username) {
-        super(username);
-    }
-
-    @Override
-    public Role getRole() {return Role.ROLE_NURSE;}
-}
+public class Nurse extends User
